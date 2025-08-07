36a54b600c28412989fb861dbb9f502c33195389
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2025-08-07 13:50:38 +0900

    [Init] Terraform 초기 설정 및 Oracle Cloud 연동 (#1)

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "7.12.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.api_fingerprint
+  private_key_path = var.api_private_key_path
+  region           = var.region
+}
