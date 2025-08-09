d8691d2bb61384f44105ee909245fed7dc726cf8
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2025-08-09 18:58:20 +0900

    Edit variables.tf; 가상 클라우드 네트워크(VCN) 관련 변수 추가

--- variables.tf
+++ variables.tf
@@ -27,3 +27,44 @@ variable "region" {
   description = "OCI region where resources will be created for Tenancy"
   type        = string
 }
+
+# VCN specific variables
+variable "create_new_vcn" {
+  description = "Boolean variable to specify whether to create a new VCN or to reuse an existing one."
+  type        = bool
+}
+
+variable "compartment_id" {
+  description = "OCI compartment where resources will be created"
+  type        = string
+}
+
+variable "vcn_cidr_block" {
+  description = "The list of IPv4 CIDR blocks the VCN will use"
+  type        = string
+}
+
+variable "vcn_display_name" {
+  description = "provide a descriptive name for the VCN - this is what you will see displayed in the OCI console"
+  type        = string
+}
+
+variable "vcn_dns_label" {
+  description = "provide a descriptive alphanumeric name for the DNS - this is what you will see displayed in the OCI console"
+  type        = string
+}
+
+variable "vcn_id" {
+  description = "provide your existing VCN OCID if create_new_vcn = false"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "provide existing private subnet OCID"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "provide existing public subnet OCID"
+  type        = string
+}
