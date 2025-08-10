897130b056bddfe508588ed31ff42ba731c71e3b
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2025-08-10 15:25:47 +0900

    Edit networking.tf; 퍼블릭 및 프라이빗 서브넷(Subnet)을 생성하기 위한 리소스 정의

--- networking.tf
+++ networking.tf
@@ -6,3 +6,24 @@ resource "oci_core_vcn" "shl-VCN" {
   display_name = var.vcn_display_name
   dns_label    = var.vcn_dns_label
 }
+
+resource "oci_core_subnet" "private_subnet" {
+  count          = (var.create_new_vcn) ? 1 : 0
+  cidr_block     = var.private_subnet_cidr_block
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.shl-VCN.*.id[0]
+
+  display_name               = var.private_subnet_display_name
+  prohibit_public_ip_on_vnic = var.private_subnet_prohibit_public_ip_on_vnic
+}
+
+resource "oci_core_subnet" "public_subnet" {
+  count          = (var.create_new_vcn) ? 1 : 0
+  cidr_block     = var.public_subnet_cidr_block
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.shl-VCN.*.id[0]
+
+  display_name               = var.public_subnet_display_name
+  prohibit_public_ip_on_vnic = var.public_subnet_prohibit_public_ip_on_vnic
+  route_table_id             = oci_core_route_table.shl_route_table.*.id[0]
+}
